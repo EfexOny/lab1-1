f79dc855184a1c65ef629362c9e3ec1e96978ee8
EfexOny <irimiamihai2006@yahoo.com>
2026-05-20 23:51:32 +0300

    factorial

---
+++ lab/main.py
@@ -0,0 +1,59 @@
+from threading import Thread, Condition
+import time
+
+# elemente = []
+# conditie = Condition()
+
+class Consumator(Thread):
+    def __init__(self,condition,elements):
+        Thread.__init__(self)
+        self.conditie = condition
+        self.elemente = elements
+    
+    def consumator(self):
+        self.conditie.acquire()
+        if len(self.elemente) == 0:
+            self.conditie.wait()
+            print('mesaj de la consumator: nu am nimic disponibil')
+        self.elemente.pop()
+        print('mesaj de la consumator : am utlizat un element')
+        print('mesaj de la consumator : mai am disponibil', len(self.elemente), 'elemente')
+        self.conditie.notify()
+        self.conditie.release()
+
+    def run(self):
+        for i in range(5):
+            self.consumator()
+   
+class Producator(Thread):
+    def __init__(self,conditie,elemente):
+        Thread.__init__(self)
+        self.conditie = conditie
+        self.elemente = elemente
+    
+    def producator(self):
+        self.conditie.acquire()
+        if len(self.elemente) == 10:
+            self.conditie.wait()
+            print('mesaj de la producator : am disponibile', len(self.elemente), 'elemente')
+            print('mesaj de la producator : am oprit productia')
+        self.elemente.append(1)
+        print('mesaj de la producator : am produs', len(self.elemente), 'elemente')
+        self.conditie.notify()
+        self.conditie.release()
+
+    def run(self):
+        for i in range(5):
+            self.producator()
+
+if __name__ == '__main__':
+    conditie_thread = Condition()
+    elemente_thread = []
+
+    producator = Producator(conditie_thread,elemente_thread)
+    consumator = Consumator(conditie_thread,elemente_thread)
+
+    producator.start()
+    consumator.start()
+    producator.join()
+    consumator.join()
--- lab11/factorial.py
+++ lab11/factorial.py
@@ -7,6 +7,7 @@ folosind atât multiprocessing.Queue + Process cât și ProcessPoolExecutor.
 
 import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
+import math
 
 
 # TODO: Implementează funcția factorial
@@ -27,7 +28,11 @@ def factorial(n: int) -> int:
         factorial(1) == 1
         factorial(5) == 120
     """
-    raise NotImplementedError("De implementat")
+    if(n < 0 ):
+        raise ValueError("n este negativ")
+    if(n==0 or n==1):
+        return 1
+    return n*factorial(n-1)
 
 
 def _worker_factorial(input_queue: multiprocessing.Queue, output_queue: multiprocessing.Queue) -> None:
@@ -43,7 +48,11 @@ def _worker_factorial(input_queue: multiprocessing.Queue, output_queue: multipro
         output_queue: Coada unde se trimit perechile (n, rezultat).
     """
     # TODO: Implementează bucla worker
-    raise NotImplementedError("De implementat")
+    val = input_queue.get()
+    if(val is None):
+        return 
+    rez = math.factorial(val)
+    output_queue.put((val,rez))
 
 
 # TODO: Implementează funcția parallel_factorial_multiprocessing
@@ -62,7 +71,43 @@ def parallel_factorial_multiprocessing(values: list[int]) -> dict[int, int]:
         result = parallel_factorial_multiprocessing([5, 6, 7, 8])
         # {5: 120, 6: 720, 7: 5040, 8: 40320}
     """
-    raise NotImplementedError("De implementat")
+    q1 = multiprocessing.Queue()
+    q2 = multiprocessing.Queue()
+
+    q1.put(values[0])
+    q1.put(values[1])
+    q1.put(values[2])
+    q1.put(values[3])
+
+    w1 = multiprocessing.Process(target=_worker_factorial,args=(q1,q2))
+    w2 = multiprocessing.Process(target=_worker_factorial,args=(q1,q2))
+    w3 = multiprocessing.Process(target=_worker_factorial,args=(q1,q2))
+    w4 = multiprocessing.Process(target=_worker_factorial,args=(q1,q2))
+
+    w1.start()
+    w2.start()
+    w3.start()
+    w4.start()
+
+    r1 = q2.get()
+    r2 = q2.get()
+    r3 = q2.get()
+    r4 = q2.get()
+
+    res = {
+        r1[0]:r1[1],
+        r2[0]:r2[1],
+        r3[0]:r3[1],
+        r4[0]:r4[1],
+    }
+
+    w1.join()
+    w2.join()
+    w3.join()
+    w4.join()
+
+    return res;
+
 
 
 # TODO: Implementează funcția parallel_factorial_futures
@@ -81,4 +126,29 @@ def parallel_factorial_futures(values: list[int]) -> dict[int, int]:
         result = parallel_factorial_futures([5, 6, 7, 8])
         # {5: 120, 6: 720, 7: 5040, 8: 40320}
     """
-    raise NotImplementedError("De implementat")
+    with ProcessPoolExecutor(max_workers=4) as w:
+        res1 = w.submit(_worker_factorial,values[0])
+        res2 = w.submit(_worker_factorial,values[1])
+        res3 = w.submit(_worker_factorial,values[2])
+        res4 = w.submit(_worker_factorial,values[3])
+
+    r1 = res1.result()
+    r2 = res2.result()
+    r3 = res3.result()
+    r4 = res4.result()
+
+
+
+    rez = {
+    r1[0]:r1[1],
+    r2[0]:r2[1],
+    r3[0]:r3[1],
+    r4[0]:r4[1],
+    }
+
+    res1.join()
+    res2.join()
+    res3.join()
+    res4.join()
+
+    return rez
